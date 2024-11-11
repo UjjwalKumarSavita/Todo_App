53d0b1b7fe998861d76c96c745f6b186c476eb24
UjjwalKumarSavita <ujjwalsavita993534@gmail.com>
2024-11-11 11:53:55 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
 import authRouter from './routes/auth.js';
-import cors from 'cors'
+import cors from 'cors';
 import todoRouter from './routes/todo.js';
-
 
 const app = express();
 app.use(express.json());
@@ -15,36 +13,36 @@ const allowedOrigins = ['https://w3todo.vercel.app'];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  credentials: true, // Add if credentials (like cookies) are needed
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Enable preflight for all routes
 
 const PORT = process.env.PORT || 3000;
 main().catch((err) => console.log(err));
+
 async function main() {
   try {
     await mongoose.connect(process.env.DB_URL);
-
     console.log("Connected to database....");
   } catch (error) {
     console.log("Connection failed", error);
   }
 }
 
-app.use('/auth',authRouter);
-app.use('/todo',todoRouter);
+app.use('/auth', authRouter);
+app.use('/todo', todoRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, Express with ES6 Modules!');
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);

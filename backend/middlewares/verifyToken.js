62b56ee15e9dae936ad29cb925e1a2c05217ce74
UjjwalKumarSavita <105811768+UjjwalKumarSavita@users.noreply.github.com>
2024-11-17 02:18:41 +0530

    Update verifyToken.js

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -15,7 +15,7 @@ export const verifyToken = (req, res, next) => {
   }
 
   try {
-    console.log("bhsdk");
+    
     
     // Verify token and attach decoded info to req.user
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
